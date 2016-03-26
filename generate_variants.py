21f0abfb72ed0bc12be95ce5aa43089872423435
Michiel Scholten <michiel@diginaut.net>
2016-03-26 15:29:52 +0100

    Generate colour variants from -base

---
+++ generate_variants.py
@@ -0,0 +1,11 @@
+import os
+import stat
+import sys
+from utilkit import fileutils
+
+variants = {
+        'grey': {'theme-colour': '#616161', 'base_colour': 'grey', 'highlight_colour': 'orange'}
+        }
+
+
+for 
