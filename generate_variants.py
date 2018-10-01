bb3d5f209bbd96a05c2349fe02bab089b9b73cd7
Michiel Scholten <michiel@diginaut.net>
2018-10-01 12:08:14 +0200

    Better indentation

--- generate_variants.py
+++ generate_variants.py
@@ -13,31 +13,34 @@ CACHEBUSTER = str(int(datetimeutil.python_to_unix(TIMESTAMP)))
 files_to_rename = {'style.css': 'style_' + CACHEBUSTER + '.css'}
 
 variants = {
-        'green': {'theme_colour': '#1b5e20', 'base_colour': 'green lighten-5', 'base_text': 'black',
-                'nav_colour': 'green darken-4',
-                'header_align': '', 'header_text': 'green-text text-darken-4',
-                'card_colour': 'green darken-4', 'card_text': 'white-text',
-                'footer_colour': 'green darken-4', 'footer_text': 'green-text text-lighten-4', 'linklist_text': 'white-text',
-                'highlight_colour': 'green-text text-darken-4',
-                'link_text': '#ff9800', 'block_colour': '#c8e6c9', 'block_text': '#000',
-            },
-        'grey': {'theme_colour': '#616161', 'base_colour': 'grey lighten-5', 'base_text': 'black',
-                'nav_colour': 'grey darken-2',
-                'header_align': 'center', 'header_text': 'orange-text',
-                'card_colour': 'grey darken-2', 'card_text': 'white-text',
-                'footer_colour': 'grey darken-2', 'footer_text': 'grey-text text-lighten-4', 'linklist_text': 'white-text',
-                'highlight_colour': 'orange',
-                'link_text': '#2196f3', 'block_colour': '#ddd', 'block_text': '#000',
-            },
-        'graphite': {'theme_colour': '#424242', 'base_colour': 'grey darken-2', 'base_text': 'grey-text text-lighten-2',
-                'nav_colour': 'grey darken-4',
-                'header_align': '', 'header_text': 'orange-text',
-                'card_colour': 'teal darken-4', 'card_text': 'white-text',
-                'footer_colour': 'grey darken-4', 'footer_text': 'grey-text text-lighten-4', 'linklist_text': 'white-text',
-                'highlight_colour': 'teal-text text-lighten-3',
-                'link_text': '#ffab40', 'block_colour': '#212121', 'block_text': '#e0e0e0',
-            },
-        }
+    'green': {
+        'theme_colour': '#1b5e20', 'base_colour': 'green lighten-5', 'base_text': 'black',
+        'nav_colour': 'green darken-4',
+        'header_align': '', 'header_text': 'green-text text-darken-4',
+        'card_colour': 'green darken-4', 'card_text': 'white-text',
+        'footer_colour': 'green darken-4', 'footer_text': 'green-text text-lighten-4', 'linklist_text': 'white-text',
+        'highlight_colour': 'green-text text-darken-4',
+        'link_text': '#ff9800', 'block_colour': '#c8e6c9', 'block_text': '#000',
+    },
+    'grey': {
+        'theme_colour': '#616161', 'base_colour': 'grey lighten-5', 'base_text': 'black',
+        'nav_colour': 'grey darken-2',
+        'header_align': 'center', 'header_text': 'orange-text',
+        'card_colour': 'grey darken-2', 'card_text': 'white-text',
+        'footer_colour': 'grey darken-2', 'footer_text': 'grey-text text-lighten-4', 'linklist_text': 'white-text',
+        'highlight_colour': 'orange',
+        'link_text': '#2196f3', 'block_colour': '#ddd', 'block_text': '#000',
+    },
+    'graphite': {
+        'theme_colour': '#424242', 'base_colour': 'grey darken-2', 'base_text': 'grey-text text-lighten-2',
+        'nav_colour': 'grey darken-4',
+        'header_align': '', 'header_text': 'orange-text',
+        'card_colour': 'teal darken-4', 'card_text': 'white-text',
+        'footer_colour': 'grey darken-4', 'footer_text': 'grey-text text-lighten-4', 'linklist_text': 'white-text',
+        'highlight_colour': 'teal-text text-lighten-3',
+        'link_text': '#ffab40', 'block_colour': '#212121', 'block_text': '#e0e0e0',
+    },
+}
 
 
 templates = ['androidapp.html', 'base.html', 'gallery.html', 'galleryitem.html', 'news.html', 'css/style.css']
